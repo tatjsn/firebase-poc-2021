ab57c03d75551b391966a931130bf4e5010de5b8
Taz Jackson <tatjsn@users.noreply.github.com>
2021-09-29 11:34:07 +0100

    Reorganise

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 
-function App() {
+function App({ db }) {
+  const [items, setItems] = useState(null);
+  const [id, setId] = useState(null);
+  const [like, setLike] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const col = collection(db, 'eirei');
+      const docsSnapshot = await getDocs(col);
+      setItems(docsSnapshot.docs);
+    }
+    fetchData();
+  }, [db]);
+
+  useEffect(() => {
+    if (!id) return null;
+    setLike(null);
+    const unsub = onSnapshot(doc(db, 'like', id), (doc) => {
+      setLike(doc.data().like);
+    });
+    return unsub;
+  }, [db, id]);
+
+  if (!items) {
+    return (
+      <p>Now loading...</p>
+    );
+  }
+
+  if (!id) {
+    return (
+      <ul>
+        { items.map(item => (
+          <li key={item.id}>
+            <button onClick={() => setId(item.id)}>{item.data().name}</button>
+          </li>
+        )) }
+      </ul>
+    );
+  }
+
+  const item = items.find(i => i.id === id);
+  const addLike = () => updateDoc(doc(db, 'like', id), { like: like + 1 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{item.data().name}</p>
+      <p><a href={item.data().url}>Article</a></p>
+      <p>Like: {like} <button onClick={addLike}>Add</button></p>
+      <p><button onClick={() => setId(null)}>Back</button></p>
     </div>
   );
 }
